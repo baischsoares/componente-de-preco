2a771020ec591ae395f3e4eeeda79c2ee2f1d55e
baischsoares <baischsoares@gmail.com>
2022-09-12 21:39:31 -0300

    projeto de um componente de preço interativo criado para fixar conteúdo

---
+++ script.js
@@ -0,0 +1,15 @@
+let valor = document.querySelector('input[type="range"]');
+let anual = document.querySelector('#anual');
+let preco = document.querySelector('.preco');
+
+valor.addEventListener('change', alterarValor)
+anual.addEventListener('change', alterarValor)
+
+function alterarValor(){
+  preco.innerText = valor.value;
+  if(anual.checked){
+    let valorFinal = valor.value * 0.25;
+    console.log(valorFinal)
+    preco.innerText = valorFinal
+  } 
+}
